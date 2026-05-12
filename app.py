d9c45c8ebf2fd7bdef63f255448e1183bc78c75c
parisath <parisath213@gmail.com>
2026-05-12 10:48:47 +0530

    without env

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/1")
+def work():
+    return "hello"
+
+
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
